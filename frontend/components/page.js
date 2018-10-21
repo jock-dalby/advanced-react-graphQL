97fe9a2c38e388ef61d096b6e62cc86906cc386d
Jock Dalby <jockdalby86@gmail.com>
2018-10-22 06:50:41 +1300

    remove example of how to create a button element using styled-components

--- frontend/components/page.js
+++ frontend/components/page.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import Meta from './Meta';
-import styled from 'styled-components';
-
-const MyButton = styled.button`
-  background: red;
-  font-size: ${props => props.huge ? '100px' : '50px'};
-`;
+import Meta from '../components/Meta';
 
 class Page extends Component {
   render() {
@@ -14,8 +8,6 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
-        <MyButton huge>Click Me</MyButton>
-        <MyButton>Click Me</MyButton>
         {this.props.children}
       </div>
     )
