f95266e84642de9e9de40566704091017e5de41e
Jock Dalby <jockdalby86@gmail.com>
2018-10-27 18:51:18 +1300

    set up example Query(getDogs) and Mutation(createDog) in schema and resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,24 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, context, info) {
+    // Just for example purposes using global to save data.
+    // Never to be used in real app.
+    global.dogs = global.dogs || [];
+    const newDog = { name: args.name }
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
+
+/**
+ * Example mutation in playground
+
+ If have more than one query or mutation in playground, can name them to choose which one to run
+
+  mutation createADog {
+    createDog(name: "Snickers") {
+      name
+    }
+  }
+*/
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,25 @@
-const Query = {};
+const Query = {
+  // parent: the parent schema
+  // arguements that have been passed to the query
+  // context: way to access the DB and rest of the incoming request (headers, cookies etc.)
+  // info: info around GraphQL query coming in
+  dogs(parent, args, context, info) {
+    // Just for example purposes using global to save data.
+    global.dogs = global.dogs || [{ name: 'Alfie' }, { name: 'Mac' }];
+    return global.dogs;
+  }
+};
+
+/**
+ * Example query in playground
+
+ If have more than one query or mutation in playground, can name them to choose which one to run
+ 
+ query getAllDogs {
+    dogs {
+      name
+    }
+  }
+ */
 
 module.exports = Query;
