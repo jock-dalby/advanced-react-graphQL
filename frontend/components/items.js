2c11d904aa07988a7e1acec4d9cb78fe220c2107
Jock Dalby <jockdalby86@gmail.com>
2018-10-31 17:27:54 +1300

    destructure ALL_ITEMS_QUERY payload into data, error and loading and render conditional logic to the screen from these args

--- frontend/components/items.js
+++ frontend/components/items.js
@@ -21,9 +21,12 @@ class items extends Component {
       <div>
         <p>Items!</p>
         <Query query={ALL_ITEMS_QUERY}>
-          {payload => {
-            console.log(payload);
-            return <p>Hey, child</p>
+          {/* { payload => { */}
+          {/* Desturctured to */}
+          {({data, error, loading}) => {
+            if (loading) return <p>Loading...</p>
+            if (error) return <p>Error: {error.message}</p>
+            return <p>I found {data.items.length} items!</p>
           }}
         </Query>
       </div>
