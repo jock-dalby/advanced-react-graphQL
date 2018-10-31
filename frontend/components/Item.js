965bb303cfdce6ee5330f505dffe638f746288a7
Jock Dalby <jockdalby86@gmail.com>
2018-10-31 18:27:36 +1300

    add content to item component to display price, image, description etc.

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import Item from './styles/ItemStyles';
+import formatMoney from '../lib/formatMoney';
 
 class item extends Component {
 
@@ -21,7 +24,24 @@ class item extends Component {
     return (
       <div>
         <ItemStyles>
-          <Title>{item.title}</Title>
+          {item.image && <img src={item.image} alt={item.title} />}
+          <Title>
+            <Link href={{
+              pathname: 'item',
+              query: { id: item.id }
+            }}><a>{item.title}</a></Link>
+          </Title>
+          <PriceTag>{formatMoney(item.price)}</PriceTag>
+          <p>{item.description}</p>
+
+          <div className="buttonList">
+            <Link href={{
+              pathname: 'update',
+              query: { id: item.id }
+            }}><a>Edit</a></Link>
+            <button>Add to cart</button>
+            <button>Delete</button>
+          </div>
         </ItemStyles>
       </div>
     );
