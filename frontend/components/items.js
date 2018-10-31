bdd4b634ae818886ad75d0416ad26439ffad5d28
Jock Dalby <jockdalby86@gmail.com>
2018-10-31 17:22:49 +1300

    setup ALL_ITEMS_QUERY, add to render method and log query results to console

--- frontend/components/items.js
+++ frontend/components/items.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
-
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
 
 class items extends Component {
   render() {
     return (
       <div>
         <p>Items!</p>
+        <Query query={ALL_ITEMS_QUERY}>
+          {payload => {
+            console.log(payload);
+            return <p>Hey, child</p>
+          }}
+        </Query>
       </div>
     );
   }
