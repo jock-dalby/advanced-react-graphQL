1014c0ffa76487e92713959e6b487a748b8aeb2b
Jock Dalby <jockdalby86@gmail.com>
2018-11-06 18:33:56 +1300

    setup update page and route and updateItem component

---
+++ frontend/components/UpdateItem.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Router from 'next/router'
+
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+import ErrorMessage from './ErrorMessage';
+
+const UPDATE_ITEM_MUTATION = gql`
+  mutation UPDATE_ITEM_MUTATION(
+    # mutation take arguments, saves them as typed variables
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem (
+      # mutation calls createItem and defines what properties is passing in and uses
+      # the values pulled in from the variables created above
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      # define what properties we want to get back
+      id
+    }
+  }
+`;
+
+class UpdateItem extends Component {
+  state = {
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0,
+  }
+
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === 'number' ? parseFloat(value) : value;
+    this.setState({ [name]: val })
+  }
+
+  render() {
+    return (
+      <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+        {/* { (mutationFunction, payload) => { */}
+        {/* Destructured and renamed to */}
+        {/* called is boolean if has been called */}
+        {(createItem, { loading, error, called, data}) => {
+          return (
+          // TODO: Stop form from being submitted while image file is being uploaded to cloudinary
+          <Form onSubmit={async e => {
+              // stop form from submitting
+              e.preventDefault();
+              // call the mutation
+              const response = await createItem();
+              // take them to the single item page fro newly created item
+              Router.push({
+                pathname: '/item',
+                query: { id: response.data.createItem.id }
+              })
+            }}>
+            <ErrorMessage error={error}/>
+            <fieldset disabled={loading} aria-busy={loading}>
+
+              <label htmlFor="title">
+                Title
+                <input type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                  required />
+              </label>
+  
+              <label htmlFor="price">
+                Price
+                <input type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                  required />
+              </label>
+  
+              <label htmlFor="description">
+                Description
+                <textarea id="description"
+                  name="description"
+                  placeholder="Description"
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                  required />
+              </label>
+              
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>)
+        }}
+      </Mutation>
+    );
+  }
+}
+
+export default UpdateItem;
+export { UPDATE_ITEM_MUTATION };
---
+++ frontend/pages/update.js
@@ -0,0 +1,12 @@
+import UpdateItem from '../components/UpdateItem';
+
+const Update = props => {
+  return (
+    <div>
+      <UpdateItem id={props.query.id} />
+    </div>
+  )
+};
+
+
+export default Update;
