47a8ffb9f740a043bca573cbe62f2762cf52aaed
Jock Dalby <jockdalby86@gmail.com>
2018-11-10 09:19:24 +1300

    use apollo update method on Mutation to update cache to be in sync with server after an item has been deleted, therefor updating the UI without need to refresh

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { ALL_ITEMS_QUERY } from './Items';
 
 const DELETE_ITEM_MUTATION = gql`
   mutation DELETE_ITEM_MUTATION($id: ID!) {
@@ -11,12 +12,27 @@ const DELETE_ITEM_MUTATION = gql`
 `
 
 class DeleteItem extends Component {
+  // Apollo will pass in 2 args when the Mutation updates
+  // 1. access to the cache
+  // 2. payload that came back from the Mutation (e.g. item that was deleted)
+  // This enables us to manually update the cache on the client so it matches the server.
+  update = (cache, payload) => {
+    // read the cache for the items we want
+    const cachedItems = cache.readQuery({ query: ALL_ITEMS_QUERY })
+    // filter the deleted item out of cache
+    const filteredCacheItems = cachedItems.items.filter(item => item.id !== payload.data.deleteItem.id)
+    // put filtered items back into cache
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data: { items: filteredCacheItems }})
+  }
   render() {
     return (
-      <Mutation mutation={DELETE_ITEM_MUTATION} variables={{ id: this.props.id }}>
+      <Mutation mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+        >
         {(deleteItem, { error }) => (
           <button onClick={() => {
-            if(confirm('Are you sure you want to delete this item')) {
+            if (confirm('Are you sure you want to delete this item')) {
               deleteItem();
             }
           }}>{this.props.children}</button>
