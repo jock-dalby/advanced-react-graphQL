fd63301f7c77b3dce2d035517f3215a2983ed611
Jock Dalby <jockdalby86@gmail.com>
2018-11-12 14:19:17 +1300

    setup paginator to scroll through pages using params

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import Head from 'next/head';
+import Link from 'next/link';
 import PaginationStyles from './styles/PaginationStyles';
 import { perPage } from '../config';
 
@@ -22,9 +24,36 @@ const Pagination = props => (
       const count = data.itemsConnection.aggregate.count;
       // Divide total numbe rof items by items per page (from config file) and round up to nearest whole
       const pages = Math.ceil(count / perPage);
+      const page = props.page;
       return (
         <PaginationStyles>
+          <Head>
+            <title>
+              Sick fits! | Page {page} of {pages} 
+            </title>
+          </Head>
+          {/* prefetch will load link before it needs it so will be super fast for user */}
+          {/* can add prefetch to any links and makes website much quicker for user */}
+          {/* prefetch does not work in dev mode but will work in prod */}
+          <Link 
+            prefetch
+            href={{
+            pathname: 'items',
+            query: { page: page - 1 }
+          }}>
+            <a className="prev" aria-disabled={page <= 1}>&#8592; Prev</a>
+          </Link>
           <p>Page {props.page} of {pages}</p>
+          <p>Total items: {count}</p>
+          <Link 
+            prefetch
+            href={{
+            pathname: 'items',
+            query: { page: page + 1 }
+          }}>
+            <a className="next" aria-disabled={page >= pages}>&#8594; Next</a>
+          </Link>
+          
         </PaginationStyles>
       );
     }}
