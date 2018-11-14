6b9c7b4b56874aae8f76a4d22a66555859b09005
Jock Dalby <jockdalby86@gmail.com>
2018-11-15 04:55:36 +1300

    create User 'query component'

---
+++ frontend/components/User.js
@@ -0,0 +1,39 @@
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+
+const CURRENT_USER_QUERY = gql`
+  me {
+    id
+    email
+    name
+    permissions
+  }
+`
+
+const User = props => (
+  // Need to get props and manually pass down to children
+  <Query query={CURRENT_USER_QUERY} {...props}>
+    {payload => props.children(payload)}
+  </Query>
+)
+
+/**
+ * This will allow us to reuse this query and 'query component' as below
+ * 
+ * <User>
+ *  {user => (
+ *    <p>user.name</p>
+ *  )}
+ * </User>
+ */
+
+ User.PropTypes = {
+   // The only thing that must be passed as a child is a function
+   children: PropTypes.func.isRequired,
+ }
+
+ // Export query component as default export
+ export default User;
+ // Export query as named export
+ export { CURRENT_USER_QUERY };
