8797a2680ae2c54333d94b981bc1766839a99d3f
Jock Dalby <jockdalby86@gmail.com>
2018-11-26 11:07:21 +1300

    update permissions in local state

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -4,6 +4,7 @@ import ErrorMessage from './ErrorMessage';
 import Table from './styles/Table';
 import SickButton from './styles/SickButton';
 import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
 
 const possiblePermissions = [
   'ADMIN',
@@ -38,11 +39,11 @@ const Permissions = () => (
                 <tr>
                   <th>Name</th>
                   <th>Email</th>
-                  {possiblePermissions.map(permission => <th>{permission}</th>)}
+                  {possiblePermissions.map(permission => <th key={permission}>{permission}</th>)}
                   <th>👇🏻</th>
                 </tr>
               </thead>
-              <tbody>{data.users.map(user => <User user={user} />)}</tbody>
+              <tbody>{data.users.map(user => <UserPermissions key={user.id} user={user} />)}</tbody>
             </Table>
           </div>
         </div>
@@ -51,7 +52,40 @@ const Permissions = () => (
   </Query>
 );
 
-class User extends React.Component {
+class UserPermissions extends React.Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      id: PropTypes.string,
+      permissions: PropTypes.array
+    }).isRequired,
+  };
+
+  state = {
+    // populating intial state using props is a no-no in React because if they change
+    // at a higher level after component has been created, the state will not be updated.
+    // In this situation it is fine because we are seeding the data and then updating
+    // when user presses update.
+    permissions: this.props.user.permissions,
+  }
+
+  handlePermissionChange = e => {
+    const checkbox = e.target;
+    // take a copy of current permissions
+    let updatedPermissions = [...this.state.permissions];
+
+    if (checkbox.checked) {
+      updatedPermissions.push(checkbox.value);
+    } else {
+      updatedPermissions = updatedPermissions.filter(permission => permission !== checkbox.value)
+    }
+    console.log(updatedPermissions)
+    this.setState({
+      permissions: updatedPermissions
+    })
+  }
+
   render() {
     const user = this.props.user;
     return (
@@ -59,9 +93,13 @@ class User extends React.Component {
         <td>{user.name}</td>
         <td>{user.email}</td>
         {possiblePermissions.map(permission => (
-          <td>
+          <td key={permission}>
             <label htmlFor={`${user.id}-permission-${permission}`}>
-              <input type="checkbox" />
+              <input type="checkbox"
+                checked={this.state.permissions.includes(permission)}
+                value={permission}
+                onChange={this.handlePermissionChange}
+                />
             </label>
           </td>
         ))}
