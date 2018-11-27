e8a737193b2ebc89d5e0d77919d215c38bbcaeb3
Jock Dalby <jockdalby86@gmail.com>
2018-11-27 14:36:50 +1300

    setup addToCart item and mutation, create CartItem data model and create relationships between User and Item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -247,6 +247,45 @@ const Mutations = {
         id: args.userId
       },
     }, info)
+  },
+
+  async addToCart(parent, args, ctx, info) {
+    // 1. Make sure they are signed in
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('You need to be logged in');
+    }
+    // 2. Query the users current cart
+    // destructure the array to take first item that comes back, because we do not expect multiple results
+    const [existingCartItem] = ctx.db.query.cartItems({
+      // where this user
+      user: { id: userId },
+      // has this item in their cart
+      item: { id: args.id }
+    })
+    // 3. Check if that item is already in their cart and increment by 1 if is
+    if(existingCartItem) {
+      console.log('This item is already in the cart');
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1}
+      })
+    }
+    // 4. If it is not, create a fresh CartItem
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {
+          connect: {
+            id: userId
+          }
+        },
+        item: {
+          connect: {
+            id: args.id
+          }
+        }
+      }
+    })
   }
 };
 
