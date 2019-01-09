fedaab52e80ddb94644959189a1186a1fefab064
Jock Dalby <jockdalby86@gmail.com>
2019-01-09 17:17:42 +1300

    set up query to manually query apollo client when autocomplete value changes

--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -6,14 +6,60 @@ import gql from 'graphql-tag';
 import debounce from 'lodash.debounce';
 import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
 
+const SEARCH_ITEMS_QUERY = gql`
+  query SEARCH_ITEMS_QUERY($searchTerm: String!) {
+    items(where: {
+      OR: [
+        { title_contains: $searchTerm },
+        { description_contains: $searchTerm }
+      ]
+    }) {
+      id
+      image
+      title
+    }
+  }
+`;
+
 class AutoComplete extends Component {
+  state = {
+    items: [],
+    loading: false
+  }
+
+  onChange = debounce(async (e, client ) => {
+    // turn loading on
+    this.setState({ loading: true });
+    // manually query apollo client
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: { searchTerm: e.target.value }
+    })
+    this.setState({
+      items: res.data.items,
+      loading: false
+    });
+  }, 350)
+
   render() {
     return (
       <SearchStyles>
         <div>
-          <input type="search" />
+          <ApolloConsumer>
+            {(client) => (
+              <input type="search" onChange={e => {
+                e.persist();
+                this.onChange(e, client);
+              }}/>
+            )}
+          </ApolloConsumer>
           <DropDown>
-            <p>Items will go here</p>
+            {this.state.items.map(item => (
+              <DropDownItem key={item.id}>
+                <img width="50" src={item.image} alt={item.title} />
+                {item.title}
+              </DropDownItem>
+            ))}
           </DropDown>
         </div>
       </SearchStyles>
